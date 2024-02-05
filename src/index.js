29c104fa2ca06fc1b629c35c360cdc212ee2c7c8
Nicholas <urkona@list.ru>
2024-02-05 14:00:30 +0300

    feat: add goodbye

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+import readline from "readline";
+
 const args = process.argv.slice(2);
 const usernameArgIndex = args.findIndex((arg) => arg.startsWith("--username="));
 const username =
   usernameArgIndex !== -1 ? args[usernameArgIndex].split("=")[1] : "Unknown";
+const start = async () => {
+  const args = process.argv.slice(2);
+  const username =
+    args.length > 0 && args[0].startsWith("--username")
+      ? args[0].split("=")[1]
+      : "Mystery Explorer";
 
-console.log(`Welcome to the File Manager, ${username}!`);
+  console.log(`Welcome to the File Manager, ${username}!`);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {}).on("close", () => {
+    console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
+  });
+};
+
+start();
